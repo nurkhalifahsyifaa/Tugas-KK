d18ed97d79b9fec5c41110ff56d811afb6973ad0
nurkhalifahsyifaa <ssyifaa723@gmail.com>
2026-07-20 15:27:30 +0800

    tugas KK saya hehe

--- Kasus1.js
+++ Kasus1.js
@@ -1,13 +1,12 @@
-for (let i = 0; i < dataPelanggan.length; i++) {
-    tampilkanLaporan(
-        dataPelanggan[i].nama, 
-        dataPelanggan[i].umur, 
-        dataPelanggan[i].harga
-    );
-}
-    return jumlah;
+function hitungTotalBelanja(daftarHarga) {
+    let total = 0;
+    for (let i = 0; i < daftarHarga.length; i++) {
+        total += daftarHarga[i]; 
+    }
+    return total;
 }
 
+// 2. Fungsi laporan milikmu (SUDAH BENAR)
 function tampilkanLaporan(nama, umur, daftarHarga) {
     const totalHarga = hitungTotalBelanja(daftarHarga);
 
@@ -21,51 +20,26 @@ function tampilkanLaporan(nama, umur, daftarHarga) {
     console.log(`Nama        : ${nama}`);
     console.log(`Umur        : ${umur} tahun`);
     console.log(`Total Harga : Rp${totalHarga.toLocaleString()}`);
+    console.log(`Status      : ${status}`);
+    console.log("");
 }
-
 const dataPelanggan = [
     {
         nama: "Budi",
         umur: 20,
-        harga: [12000], [17500]
+        harga: [12000, 17500]
     },
     {
         nama: "Koti",
         umur: 37,
-        harga: [37800], [42500]
+        harga: [37800, 42500]
     },
 ];
 
 for (let i = 0; i < dataPelanggan.length; i++) {
-    tampilkanLaporan(dataPelanggan[i].nama, dataPelanggan[i].harga);
+    tampilkanLaporan(
+        dataPelanggan[i].nama, 
+        dataPelanggan[i].umur, 
+        dataPelanggan[i].harga
+    );
 }
-
-function hitungTotalBelanja(daftarHarga) {
-    let total = 0;
-    for (let i = 0; i < daftarHarga.length; i++) {
-        total += daftarHarga[i]; // Menjumlahkan setiap harga barang
-    }
-    return total;
-}
-
-
-function tampilkanLaporan(nama, umur, daftarHarga) {
-
-    const totalHarga = hitungTotalBelanja(daftarHarga);
-
-    let status = "";
-    if (totalHarga >= 30000) {
-        status = "Selamat! Anda mendapatkan diskon.";
-    } else {
-        status = "Belanja lagi agar mendapat diskon.";
-    }
-
-    // Output Laporan
-    console.log(`Nama        : ${nama}`);
-    console.log(`Umur        : ${umur} tahun`);
-    console.log(`Daftar Item : Rp${daftarHarga.join(', Rp')}`);
-    console.log(`Total Harga : Rp${totalHarga.toLocaleString()}`);
-    console.log(`Status      : ${status}`);
-    console.log("------------------------------------------");
-}
-
